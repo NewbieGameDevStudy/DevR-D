b02c15bd76b9458061afe86e522e5a2009d18636
namoeye <namoeye@imc.co.kr>
2017-12-25 03:42:24 +0900

    혁재 : protobuf 추가 및 자동proto 파일 변환 추가

---
+++ DevProject/ProjectQ_Server/ConsoleServer/protobuf.bat
@@ -0,0 +1,33 @@
+@echo off
+
+set protoListPath=%CD%\Packet\Protobuf\
+set protocPath=%CD%\packages\Google.Protobuf.Tools.3.5.1\tools\windows_x64\
+set rootPath=%CD%
+set SRC_DIR=%protoListPath%
+set DST_DIR=%protoListPath%
+
+cd %protoListPath%
+
+del /q "*.cs"
+
+dir /b >protoList.txt
+
+cd %protocPath%
+
+
+set protoListFile=%protoListPath%protoList.txt
+
+for /f "delims=" %%i in (%protoListFile%) do (
+
+ if %%i == protoList.txt goto END
+
+ protoc.exe -I=%SRC_DIR% --csharp_out=%DST_DIR% %SRC_DIR%/%%i
+)
+
+:END
+
+cd %protoListPath%
+
+del protoList.txt
+
+pause
