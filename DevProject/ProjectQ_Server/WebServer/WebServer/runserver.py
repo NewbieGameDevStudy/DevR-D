76aa7b7afe78e0ef2eb7c63e7fc91740af796106
namoeye <namoeye@imc.co.kr>
2018-02-04 21:52:28 +0900

    혁재 : python 테스트 커밋

--- DevProject/ProjectQ_Server/WebServer/WebServer/runserver.py
+++ DevProject/ProjectQ_Server/WebServer/WebServer/runserver.py
@@ -3,31 +3,44 @@ This script runs the WebServer application using a development server.
 """
 
 from WebServer import app
+from flask import Flask, jsonify
+from flask_restful import fields, Resource, Api, marshal_with
+from flask_restful import reqparse
 
 #print(environ.get('SERVER_HOST', 'localhost'))
 #print(environ.get('SERVER_PORT', '5555'))
 #a = app.config['MYSQL_DATABASE_USER']
 
-"""
-server host
-server port
+#app = Flask(__name__)
+#api = Api(app)
 
-DB host
-DB port
+#test = {
+#    'name': fields.String
+#}
 
-DB root
-DB password
+#odd = [1,2,3]
 
-"""
+#test_feilds = {
+#    'int' : fields.Integer,
+#}
 
-#if __name__ == "__main__":
-#    app.run(debug = __debug__)
+#class Call(object):
+#    def __init__(self, test):
+#        self.test = test
 
-"""
-if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
-    except method_name():
-        PORT = 5555
-    app.run(HOST, PORT)"""
+#class CreateUser(Resource) :
+#    #@marshal_with(test_feilds)
+#    def post(self, **kwargs):
+#        parser = reqparse.RequestParser()
+#        parser.add_argument("test");
+#        args = parser.parse_args()
+#        dd = args['test']
+#        data = {'a': 321}
+#        return jsonify(data)
+#        #return Call(test = 100)
+#    def get(self):
+#        data = {'a': 321, 'list':odd}
+#        return jsonify(data)
+
+#api.add_resource(CreateUser, '/test')
+app.run("localhost", 5000)
