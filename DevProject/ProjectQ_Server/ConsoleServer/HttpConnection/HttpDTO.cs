7ac1a0ec1b8bd6924d19a1f46d4cde2c46eeb3d7
DESKTOP-KENVP97\namoeye <oiloveyouio>
2018-05-07 21:17:57 +0900

    혁재 : 프로토콜 정의

--- DevProject/ProjectQ_Server/ConsoleServer/HttpConnection/HttpDTO.cs
+++ DevProject/ProjectQ_Server/ConsoleServer/HttpConnection/HttpDTO.cs
@@ -4,30 +4,48 @@ using System.Collections.Generic;
 
 namespace GameServer.Connection
 {
-    public class ResponseBase
+    public class Protocol
     {
-        public int responseCode;
+        #region 공용 프로토콜 정의
+        public class ResponseBase
+        {
+            public int responseCode;
+        }
+
+        #region 응답값
+        public const int RESPONSE_OK = 201;
+        #endregion
+
+        #endregion
+
+
+        #region 컨텐츠별 프로토콜
+
+        #region Login 프로토콜
+        //ERROR
+        public const int ERROR_LOGIN_NOT_FOUND_ACCOUNT = 101;
+        public const int ERROR_LOGIN_FAILED_PARAM = 102;
+        public const int ERROR_CREATE_NOT_LOGIN = 103;
+
+        //SUCCESS
+        public const int SUCCESS_CREATE_LOGIN = 2001;
+
+        [HttpConnect(Method.GET, "/loginInfo")]
+        public class ReqPlayerInfo
+        {
+            public ulong accountId;
+        }
+
+        public class RespPlayerInfo : ResponseBase
+        {
+            public bool result;
+            public int level;
+            public int exp;
+            public int gameMoney;
+            public string name;
+        }
+        #endregion
+
+        #endregion
     }
-
-    #region 오류 코드
-    //const int ERROR_
-
-    #endregion
-
-    #region 플레이어 관련 요청
-    [HttpConnect(Method.GET, "/loginInfo")]
-    public class ReqPlayerInfo
-    {
-        public ulong accountId;
-    }
-
-    public class RespPlayerInfo : ResponseBase
-    {
-        public bool result;
-        public int level;
-        public int exp;
-        public int gameMoney;
-        public string name;
-    }
-    #endregion
 }
